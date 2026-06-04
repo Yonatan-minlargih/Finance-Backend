e82e70365dc91e288be1226a1225deff0a0e9fa3
Mikias Eyayaw <mikiaseyayaw123@gmail.com>
2026-06-05 01:21:30 +0300

    final backend modifications

--- Core-Finance-service/finance-core-main/target/generated-sources/annotations/com/financial/corefinance/mapper/JournalMapperImpl.java
+++ Core-Finance-service/finance-core-main/target/generated-sources/annotations/com/financial/corefinance/mapper/JournalMapperImpl.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-06-04T15:00:54+0300",
+    date = "2026-06-05T00:25:53+0300",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.46.0.v20260407-0427, environment: Java 21.0.10 (Eclipse Adoptium)"
 )
 @Component
